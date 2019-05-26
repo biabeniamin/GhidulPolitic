e2f39460ef738fbd12077ccb4633dc818e0c5289
Beniamin Bia <biabeniamin@outlook.com>
2019-05-26 20:23:11 +0300

    working login and main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// app.module.ts
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot([
+      {
+         path: 'main',
+         component: MainComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+     },
+   ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
+
 })
 export class AppModule { }
