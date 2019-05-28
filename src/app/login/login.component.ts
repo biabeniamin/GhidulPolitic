bdb35ec51c804c5ff63052426585e6d84f01e3bb
Beniamin Bia <biabeniamin@outlook.com>
2019-05-28 13:39:39 +0300

    login handler for logion form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CandidateService } from '../CandidateService';
 
 @Component({
   selector: 'app-login',
@@ -11,5 +12,16 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  loginCandidate(event)
+	{
+		event.preventDefault();
+		const target = event.target;
+		let candidate = CandidateService.GetDefaultCandidate();
+		candidate.email = target.querySelector('#Email').value;
+		candidate.password = target.querySelector('#Password').value;
+		console.log(candidate);
+		//this.candidateService.AddCandidate(candidate);
+	}
 
 }
