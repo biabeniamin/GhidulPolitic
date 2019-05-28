8a8ff07cf4fc9ed9f883121b55d173a615020346
Beniamin Bia <biabeniamin@outlook.com>
2019-05-28 13:32:11 +0300

    handler for form submit was added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CandidateService } from '../CandidateService';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient, 
+		private candidateService : CandidateService) { }
 
   ngOnInit() {
   }
+
+  addCandidate(event)
+	{
+		event.preventDefault();
+		const target = event.target;
+		let candidate = CandidateService.GetDefaultCandidate();
+		candidate.name = target.querySelector('#Name').value;
+		candidate.email = target.querySelector('#Email').value;
+		candidate.password = target.querySelector('#Password').value;
+		candidate.description = target.querySelector('#Description').value;
+		candidate.role = target.querySelector('#Role').value;
+		console.log(candidate);
+		this.candidateService.AddCandidate(candidate);
+	}
+
 
 }
