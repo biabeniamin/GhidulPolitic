d6c7fb3e3c67721b8856255a6585eab6b00de5b5
Beniamin Bia <biabeniamin@outlook.com>
2019-05-29 09:15:39 +0300

    load candidate from database

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { CandidateService } from '../CandidateService';
+import { Candidate } from '../Models/Candidate';
 
 @Component({
   selector: 'app-details',
@@ -8,8 +12,26 @@ import { Component, OnInit } from '@angular/core';
 export class DetailsComponent implements OnInit {
 
 public userId:number = 3;
+public candidate:Candidate ;
 public image:string="http://biabeniamin.go.ro/GhidImages/3.jpg";
-  constructor() { }
+  constructor(private http:HttpClient, 
+    private candidateService : CandidateService,
+    private cookieService: CookieService) { 
+
+      this.candidate=CandidateService.GetDefaultCandidate();
+      let userId = this.cookieService.get("userId");
+      if(userId!=null)
+      {
+        console.log("getting candidate");
+        candidateService.GetCandidatesByCandidateId(userId).subscribe((data) => {
+          console.log("candidate obtained");
+          console.log(data);
+          this.candidate = data[0];
+          this.image="http://biabeniamin.go.ro/GhidImages/"+this.candidate.candidateId+".jpg";
+        });
+      }
+
+    }
 
   ngOnInit() {
   }
