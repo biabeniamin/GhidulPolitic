c93f34b2d3a5e6b5094d8ea8d29c88f112003bab
Beniamin Bia <biabeniamin@outlook.com>
2019-05-29 09:12:04 +0300

    display image

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
 
+public userId:number = 3;
+public image:string="http://biabeniamin.go.ro/GhidImages/3.jpg";
   constructor() { }
 
   ngOnInit() {
