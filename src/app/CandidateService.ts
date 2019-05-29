bd4d3899d63233705de65f96494d11c5412eb978
Beniamin Bia <biabeniamin@outlook.com>
2019-05-29 08:27:46 +0300

    updated to server put requst

--- src/app/CandidateService.ts
+++ src/app/CandidateService.ts
@@ -55,7 +55,7 @@ export class CandidateService
 	
 	UpdateCandidate(candidate)
 	{
-		return this.http.put<Candidate>(ServerUrl.GetUrl()  + "Candidates.php?cmd=updateCandidate", candidate).subscribe(candidate =>
+		return this.http.put<Candidate>(ServerUrl.GetUrl()  + "candidate", candidate).subscribe(candidate =>
 		{
 			console.log(candidate);
 			return candidate;
