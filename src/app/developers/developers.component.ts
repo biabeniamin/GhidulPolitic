de1ffc65f8bce3e157bd1f75ebfe6d58895af5f8
Beniamin Bia <biabeniamin@outlook.com>
2019-05-29 09:21:16 +0300

    set cookie when clicked explored

--- src/app/developers/developers.component.ts
+++ src/app/developers/developers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CandidateService } from '../CandidateService';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-developers',
@@ -10,9 +11,18 @@ import { CandidateService } from '../CandidateService';
 export class DevelopersComponent implements OnInit {
 
   constructor(private http:HttpClient, 
-		public candidateService : CandidateService) { }
+    public candidateService : CandidateService,
+    private cookieService: CookieService,
+    private router: Router) { }
 
   ngOnInit() {
+  }
+
+  explore(id)
+  {
+    console.log(id);
+    this.cookieService.set( 'selectedUserId', id.toString() );
+    this.router.navigate(['/details']);
   }
 
 }
